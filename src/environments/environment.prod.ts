de4f20cdd31596cee3e079c98417c027b64099de
Viki <viktorija11@gmail.com>
2022-11-05 11:49:29 +0100

    build target in anagular

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBNm_nun-092Lj3iLHzI3HNAFYr5cWH6r8',
+    authDomain: 'ng-syncvr.firebaseapp.com',
+    projectId: 'ng-syncvr',
+    storageBucket: 'ng-syncvr.appspot.com',
+    messagingSenderId: '865715622287',
+    appId: '1:865715622287:web:590b09175446d288217bfa',
+    measurementId: 'G-19J63H9RHS',
+  },
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,16 +5,15 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBNm_nun-092Lj3iLHzI3HNAFYr5cWH6r8",
-    authDomain: "ng-syncvr.firebaseapp.com",
-    projectId: "ng-syncvr",
-    storageBucket: "ng-syncvr.appspot.com",
-    messagingSenderId: "865715622287",
-    appId: "1:865715622287:web:590b09175446d288217bfa",
-    measurementId: "G-19J63H9RHS"
-  }
-
-};
+    apiKey: 'AIzaSyBNm_nun-092Lj3iLHzI3HNAFYr5cWH6r8',
+    authDomain: 'ng-syncvr.firebaseapp.com',
+    projectId: 'ng-syncvr',
+    storageBucket: 'ng-syncvr.appspot.com',
+    messagingSenderId: '865715622287',
+    appId: '1:865715622287:web:590b09175446d288217bfa',
+    measurementId: 'G-19J63H9RHS',
+  },
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
